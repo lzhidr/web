d891d0301bb833583b13daf61a6ad6dd95a6f6cc
h <lou001229@163.com>
2024-11-13 19:28:31 +0800

    tijiao

---
+++ js/lovetime.js
@@ -0,0 +1,16 @@
+function lovetime() {
+    window.setTimeout("lovetime()", 1000);
+    var seconds = 1000
+    var minutes = seconds * 60
+    var hours = minutes * 60
+    var days = hours * 24
+    var years = days * 365
+    var today = new Date()
+    var todayYear = today.getFullYear()
+    var todayMonth = today.getMonth()
+    var todayDate = today.getDate()
+    var todayHour = today.getHours()
+    var todayMinute = today.getMinutes()
+    var todaySecond = today.getSeconds()
+    // 修改这里时间即可 我的是2018.11.19
+   
